10130248ee28f0d9b97b1227f9d80aa4b6e525b3
XuatQC <xuatqc@saisytem.vn>
2020-04-03 17:27:54 +0700

    add source

---
+++ ExportSource/ExportSource/SettingFileFrm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ExportSource
+{
+ partial class chkFileXaml
+ {
+  /// <summary>
+  /// Required designer variable.
+  /// </summary>
+  private System.ComponentModel.IContainer components = null;
+
+  /// <summary>
+  /// Clean up any resources being used.
+  /// </summary>
+  /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+  protected override void Dispose(bool disposing)
+  {
+   if (disposing && (components != null))
+   {
+	components.Dispose();
+   }
+   base.Dispose(disposing);
+  }
+
+  #region Windows Form Designer generated code
+
+  /// <summary>
+  /// Required method for Designer support - do not modify
+  /// the contents of this method with the code editor.
+  /// </summary>
+  private void InitializeComponent()
+  {
+   this.chkboxSelectAll = new System.Windows.Forms.CheckBox();
+   this.label1 = new System.Windows.Forms.Label();
+   this.chkFileCs = new System.Windows.Forms.CheckBox();
+   this.chkSql = new System.Windows.Forms.CheckBox();
+   this.checkBox3 = new System.Windows.Forms.CheckBox();
+   this.chkFileXml = new System.Windows.Forms.CheckBox();
+   this.chkFileCsproj = new System.Windows.Forms.CheckBox();
+   this.chkFileXsd = new System.Windows.Forms.CheckBox();
+   this.checkBox7 = new System.Windows.Forms.CheckBox();
+   this.checkBox8 = new System.Windows.Forms.CheckBox();
+   this.checkBox9 = new System.Windows.Forms.CheckBox();
+   this.checkBox10 = new System.Windows.Forms.CheckBox();
+   this.SuspendLayout();
+   // 
+   // chkboxSelectAll
+   // 
+   this.chkboxSelectAll.AutoSize = true;
+   this.chkboxSelectAll.Location = new System.Drawing.Point(40, 40);
+   this.chkboxSelectAll.Name = "chkboxSelectAll";
+   this.chkboxSelectAll.Size = new System.Drawing.Size(74, 16);
+   this.chkboxSelectAll.TabIndex = 0;
+   this.chkboxSelectAll.Text = "Select All";
+   this.chkboxSelectAll.UseVisualStyleBackColor = true;
+   this.chkboxSelectAll.CheckedChanged += new System.EventHandler(this.chkboxSelectAll_CheckedChanged);
+   // 
+   // label1
+   // 
+   this.label1.AutoSize = true;
+   this.label1.Location = new System.Drawing.Point(22, 13);
+   this.label1.Name = "label1";
+   this.label1.Size = new System.Drawing.Size(143, 12);
+   this.label1.TabIndex = 1;
+   this.label1.Text = "Select file you want to use";
+   // 
+   // chkFileCs
+   // 
+   this.chkFileCs.AutoSize = true;
+   this.chkFileCs.Checked = true;
+   this.chkFileCs.CheckState = System.Windows.Forms.CheckState.Checked;
+   this.chkFileCs.Location = new System.Drawing.Point(80, 76);
+   this.chkFileCs.Name = "chkFileCs";
+   this.chkFileCs.Size = new System.Drawing.Size(38, 16);
+   this.chkFileCs.TabIndex = 2;
+   this.chkFileCs.Text = ".cs";
+   this.chkFileCs.UseVisualStyleBackColor = true;
+   // 
+   // chkSql
+   // 
+   this.chkSql.AutoSize = true;
+   this.chkSql.Checked = true;
+   this.chkSql.CheckState = System.Windows.Forms.CheckState.Checked;
+   this.chkSql.Location = new System.Drawing.Point(80, 111);
+   this.chkSql.Name = "chkSql";
+   this.chkSql.Size = new System.Drawing.Size(41, 16);
+   this.chkSql.TabIndex = 3;
+   this.chkSql.Text = ".sql";
+   this.chkSql.UseVisualStyleBackColor = true;
+   // 
+   // checkBox3
+   // 
+   this.checkBox3.AutoSize = true;
+   this.checkBox3.Checked = true;
+   this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+   this.checkBox3.Location = new System.Drawing.Point(80, 186);
+   this.checkBox3.Name = "checkBox3";
+   this.checkBox3.Size = new System.Drawing.Size(50, 16);
+   this.checkBox3.TabIndex = 4;
+   this.checkBox3.Text = ".xaml";
+   this.checkBox3.UseVisualStyleBackColor = true;
+   // 
+   // chkFileXml
+   // 
+   this.chkFileXml.AutoSize = true;
+   this.chkFileXml.Location = new System.Drawing.Point(80, 147);
+   this.chkFileXml.Name = "chkFileXml";
+   this.chkFileXml.Size = new System.Drawing.Size(44, 16);
+   this.chkFileXml.TabIndex = 5;
+   this.chkFileXml.Text = ".xml";
+   this.chkFileXml.UseVisualStyleBackColor = true;
+   // 
+   // chkFileCsproj
+   // 
+   this.chkFileCsproj.AutoSize = true;
+   this.chkFileCsproj.Location = new System.Drawing.Point(80, 226);
+   this.chkFileCsproj.Name = "chkFileCsproj";
+   this.chkFileCsproj.Size = new System.Drawing.Size(57, 16);
+   this.chkFileCsproj.TabIndex = 6;
+   this.chkFileCsproj.Text = ".csproj";
+   this.chkFileCsproj.UseVisualStyleBackColor = true;
+   // 
+   // chkFileXsd
+   // 
+   this.chkFileXsd.AutoSize = true;
+   this.chkFileXsd.Checked = true;
+   this.chkFileXsd.CheckState = System.Windows.Forms.CheckState.Checked;
+   this.chkFileXsd.Location = new System.Drawing.Point(251, 76);
+   this.chkFileXsd.Name = "chkFileXsd";
+   this.chkFileXsd.Size = new System.Drawing.Size(44, 16);
+   this.chkFileXsd.TabIndex = 7;
+   this.chkFileXsd.Text = ".xsd";
+   this.chkFileXsd.UseVisualStyleBackColor = true;
+   // 
+   // checkBox7
+   // 
+   this.checkBox7.AutoSize = true;
+   this.checkBox7.Location = new System.Drawing.Point(251, 111);
+   this.checkBox7.Name = "checkBox7";
+   this.checkBox7.Size = new System.Drawing.Size(40, 16);
+   this.checkBox7.TabIndex = 8;
+   this.checkBox7.Text = ".txt";
+   this.checkBox7.UseVisualStyleBackColor = true;
+   // 
+   // checkBox8
+   // 
+   this.checkBox8.AutoSize = true;
+   this.checkBox8.Location = new System.Drawing.Point(251, 147);
+   this.checkBox8.Name = "checkBox8";
+   this.checkBox8.Size = new System.Drawing.Size(38, 16);
+   this.checkBox8.TabIndex = 9;
+   this.checkBox8.Text = ".dll";
+   this.checkBox8.UseVisualStyleBackColor = true;
+   // 
+   // checkBox9
+   // 
+   this.checkBox9.AutoSize = true;
+   this.checkBox9.Location = new System.Drawing.Point(251, 186);
+   this.checkBox9.Name = "checkBox9";
+   this.checkBox9.Size = new System.Drawing.Size(47, 16);
+   this.checkBox9.TabIndex = 10;
+   this.checkBox9.Text = ".xlsx";
+   this.checkBox9.UseVisualStyleBackColor = true;
+   // 
+   // checkBox10
+   // 
+   this.checkBox10.AutoSize = true;
+   this.checkBox10.Location = new System.Drawing.Point(252, 226);
+   this.checkBox10.Name = "checkBox10";
+   this.checkBox10.Size = new System.Drawing.Size(44, 16);
+   this.checkBox10.TabIndex = 11;
+   this.checkBox10.Text = ".raw";
+   this.checkBox10.UseVisualStyleBackColor = true;
+   // 
+   // chkFileXaml
+   // 
+   this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+   this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+   this.ClientSize = new System.Drawing.Size(410, 286);
+   this.Controls.Add(this.checkBox10);
+   this.Controls.Add(this.checkBox9);
+   this.Controls.Add(this.checkBox8);
+   this.Controls.Add(this.checkBox7);
+   this.Controls.Add(this.chkFileXsd);
+   this.Controls.Add(this.chkFileCsproj);
+   this.Controls.Add(this.chkFileXml);
+   this.Controls.Add(this.checkBox3);
+   this.Controls.Add(this.chkSql);
+   this.Controls.Add(this.chkFileCs);
+   this.Controls.Add(this.label1);
+   this.Controls.Add(this.chkboxSelectAll);
+   this.Name = "chkFileXaml";
+   this.Text = "SettingFileFrm";
+   this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.chkFileXaml_FormClosed);
+   this.ResumeLayout(false);
+   this.PerformLayout();
+
+  }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkboxSelectAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkFileCs;
+        private System.Windows.Forms.CheckBox chkSql;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox chkFileXml;
+        private System.Windows.Forms.CheckBox chkFileCsproj;
+        private System.Windows.Forms.CheckBox chkFileXsd;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+    }
+}
---
+++ ExportSource/ExportSource/SettingFileFrm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportSource
+{
+ public partial class chkFileXaml : Form
+ {
+
+  // Danh sach duoi file 
+  public static HashSet<String> lstFileExt = new HashSet<string>();
+  public chkFileXaml()
+  {
+   InitializeComponent();
+  }
+
+  private void chkboxSelectAll_CheckedChanged(object sender, EventArgs e)
+  {
+   if (this.chkboxSelectAll.Checked == true)
+   {
+    foreach (Control ctrl in this.Controls)
+    {
+     CheckBox chk = ctrl as CheckBox;
+     if (chk != null)
+     {
+      chk.Checked = true;
+     }
+    }
+   }
+   else
+   {
+    foreach (Control ctrl in this.Controls)
+    {
+     CheckBox chk = ctrl as CheckBox;
+     if (chk != null)
+     {
+      chk.Checked = false;
+     }
+    }
+   }
+  }
+
+  private void chkFileXaml_FormClosed(object sender, FormClosedEventArgs e)
+  {
+
+   lstFileExt.Clear();
+   foreach (Control ctrl in this.Controls)
+   {
+    CheckBox chk = ctrl as CheckBox;
+    if (chk != null && chk.Checked == true)
+    {
+    lstFileExt.Add(chk.Text);
+    }
+   }
+  }
+
+ }
+}
